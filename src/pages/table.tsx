d4c7340f0161ddddf0d4668fb262052a516e6d62
Brian Cilenti <briancilenti@gmail.com>
2021-03-13 17:52:09 -0500

    implement react-table starter

--- src/pages/table.tsx
+++ src/pages/table.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Heading } from '@chakra-ui/react';
+import { Box, Heading, Table, Thead, Tbody, Tfoot, Tr, Th, Td, TableCaption } from '@chakra-ui/react';
 import { useTable } from 'react-table';
 
 const useTableData = () => {
@@ -26,12 +26,12 @@ const useTableData = () => {
         () => [
             {
                 Header: 'Column 1',
-                accessor: 'col1' // accessor is the "key" in the data
+                accessor: 'col1' as const // accessor is the "key" in the data
             },
 
             {
                 Header: 'Column 2',
-                accessor: 'col2'
+                accessor: 'col2' as const
             }
         ],
 
@@ -41,16 +41,40 @@ const useTableData = () => {
     return { columns, data };
 };
 
-const Table = () => {
+const MyTablePage = () => {
     const { columns, data } = useTableData();
 
-    const tableInstance = useTable({ columns, data });
+    const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = useTable({ columns, data });
 
     return (
         <Box mb={8} w="full">
             <Heading as="h1">Hello Table</Heading>
+            <Table {...getTableProps()}>
+                <Thead>
+                    {headerGroups.map((headerGroup) => (
+                        <Tr {...headerGroup.getHeaderGroupProps()}>
+                            {headerGroup.headers.map((column) => (
+                                <Th {...column.getHeaderProps()}>{column.render('Header')}</Th>
+                            ))}
+                        </Tr>
+                    ))}
+                </Thead>
+
+                <Tbody {...getTableBodyProps()}>
+                    {rows.map((row) => {
+                        prepareRow(row);
+                        return (
+                            <Tr {...row.getRowProps()}>
+                                {row.cells.map((cell) => {
+                                    return <Td {...cell.getCellProps()}>{cell.render('Cell')}</Td>;
+                                })}
+                            </Tr>
+                        );
+                    })}
+                </Tbody>
+            </Table>
         </Box>
     );
 };
 
-export default Table;
+export default MyTablePage;
