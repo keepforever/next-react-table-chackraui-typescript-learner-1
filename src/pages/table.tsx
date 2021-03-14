1c801e6c85fe7665a5f95020a56f297a220fc694
Brian Cilenti <briancilenti@gmail.com>
2021-03-13 19:01:56 -0500

    done wrestling with typescript today

--- src/pages/table.tsx
+++ src/pages/table.tsx
@@ -1,55 +1,74 @@
 import React from 'react';
-import { Box, Heading, Table, Thead, Tbody, Tfoot, Tr, Th, Td, TableCaption } from '@chakra-ui/react';
-import { useTable } from 'react-table';
+import { Box, Heading, Table, Thead, Tbody, Tr, Th, Td, TableCaption } from '@chakra-ui/react';
+import { useTable, Column } from 'react-table';
+import axios from 'axios';
 
-const useTableData = () => {
-    const data = React.useMemo(
+type UserType = {
+    address: { street: string };
+    company: { name: string };
+    email: string;
+    id: number;
+    name: string;
+    phone: string;
+    username: string;
+    website: string;
+};
+
+type ColumnType = { Header: string; accessor: string };
+
+// type TableDataType = { columns: UserType[]; data: Array<UserType> };
+type TableDataType = { columns: any; data: Array<UserType> };
+
+const useTableData = (users: Array<UserType>): TableDataType => {
+    const data = React.useMemo(() => users, []);
+
+    const columns: Array<ColumnType> = React.useMemo(
         () => [
-            {
-                col1: 'Hello',
-                col2: 'World'
-            },
-            {
-                col1: 'react-table',
-                col2: 'rocks'
-            },
-            {
-                col1: 'whatever',
-                col2: 'you want'
-            }
+            ...Object.keys(users[0]).map((key: string) => {
+                return {
+                    Header: key,
+                    accessor: key
+                };
+            })
         ],
-
-        []
-    );
-
-    const columns = React.useMemo(
-        () => [
-            {
-                Header: 'Column 1',
-                accessor: 'col1' as const // accessor is the "key" in the data
-            },
-
-            {
-                Header: 'Column 2',
-                accessor: 'col2' as const
-            }
-        ],
-
         []
     );
 
     return { columns, data };
 };
 
-const MyTablePage = () => {
-    const { columns, data } = useTableData();
+export async function getStaticProps(context: any) {
+    const res = await axios.get(`https://jsonplaceholder.typicode.com/users`);
 
+    return {
+        props: {
+            users: [
+                ...res.data.map((u: UserType) => {
+                    return {
+                        ...u,
+                        address: u.address.street,
+                        company: u.company.name
+                    };
+                })
+            ]
+        }
+    };
+}
+
+const MyTablePage = ({ users }: any) => {
+    const { columns, data } = useTableData(users);
+    // console.log('\n', '\n', `columns = `, columns, '\n', '\n');
     const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = useTable({ columns, data });
 
     return (
-        <Box mb={8} w="full">
-            <Heading as="h1">Hello Table</Heading>
+        <Box mb={8} w="full" overflowX="scroll">
+            <Heading as="h2">User Table</Heading>
+            <Heading as="h4" size="sm">
+                Fetched from https://jsonplaceholder.typicode.com/users
+            </Heading>
+
             <Table {...getTableProps()}>
+                <TableCaption>Some User Data From a Dummy API</TableCaption>
                 <Thead>
                     {headerGroups.map((headerGroup) => (
                         <Tr {...headerGroup.getHeaderGroupProps()}>
